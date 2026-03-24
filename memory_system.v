d58dd59664f739407468264760a5cba4bb683e72
Rob Gonzalez <robertgonzalez@u.boisestate.edu>
2026-03-24 10:09:49 -0600

    Edited memory_systems

--- memory_system.v
+++ memory_system.v
@@ -2,45 +2,6 @@ module memory_system(
     input [7:0] data,
     input store,
     input [1:0] addr,
-    output [7:0] memory
-);
-    // Internal wires to hold the output of each 8-bit memory block
-    wire [7:0] mem_out [3:0]; 
-    reg [3:0] selected_store;
-
-    // 1. Demultiplexer for the Store (Enable) signal
-    always @(*) begin
-        case(addr)
-            2'b00: selected_store = {3'b000, store};
-            2'b01: selected_store = {3'b00, store, 1'b0};
-            2'b10: selected_store = {3'b0, store, 2'b00};
-            2'b11: selected_store = {store, 3'b000};
-            default: selected_store = 4'b0000;
-        endcase
-    end
-
-    // 2. Instantiate 4 memory blocks using a generate loop
-    genvar i;
-    generate
-        for (i = 0; i < 4; i = i + 1) begin
-            byte_memory mem_block (
-                .data(data),
-                .store(selected_store[i]),
-                .memory(mem_out[i])
-            );
-        end
-    endgenerate
-
-    // 3. Multiplexer to select which byte to display on LEDs
-    assign memory = mem_out[addr];
-
-endmodule
-
-/** 2nd Option:
-module memory_system(
-    input [7:0] data,
-    input store,
-    input [1:0] addr,
     output [7:0] memory_out
 );
     wire [7:0] m0, m1, m2, m3;
